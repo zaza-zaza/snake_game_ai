f9893b60dbfdfb216941e16ab3e55f292dd41446
zach <zgarson@u.rochester.edu>
2024-01-08 16:37:37 -0500

    Complete agent class

--- agent.py
+++ agent.py
@@ -15,6 +15,8 @@ class Agent:
         self.epsilon = 0 # parameter to control randomness
         self.gamma = 0 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # pops elements that exceed this mem size
+        self.model = None # TODO
+        self.trainer = None # TODO
         # TODO: model, trainer
 
 
@@ -66,16 +68,37 @@ class Agent:
 
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done)) # pop left if max mem is reached
 
+
+    # The short and the long memory are used to store either one instance
+    # of the train data (short memory) or many instances at once (long mem)
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # return a list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones, = zip(*mini_sample) # groups every "states" together, "rewards" together, and so on
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        # random moves: tradeoff: exploration vs. explotaition
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
 
 def train():
     plot_scores = []
